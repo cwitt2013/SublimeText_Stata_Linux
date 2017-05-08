f0f7921691a0fd832e24a1a26af4df2b41f71222
cwitt2013 <cwittrock@posteo.de>
2017-05-08 15:42:44 +0200

    Minor Update 08.05.2017

--- sublime-stata.sh
+++ sublime-stata.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# needs wmctrl xte and xsel
-# dnf install wmctrl xdotool xautomation
-# to get them run
+# Packages: wmctrl, xdotool, xautomation
+# e.g. Fedora: dnf install wmctrl xdotool xautomation
 
 # get current window id
 winid=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 
-# check for stata window, if found active else execute
+# check for Stata window, if found active else execute
 if [ "$(pgrep stata)" ]
 then
+  # Stata Flavor?
 	wmctrl -a 'Stata/SE 14.2'
 else
-	#xstata-se &
-        xstata-se
+	# If not already open start it
+  xstata-se
 	sleep .1
 fi
 
@@ -24,10 +24,12 @@ sleep .1
 # swich to command line and select existing text via ctrl-a
 xte 'keydown Control_L' 'key 1' 'key A' 'usleep 100' 'key BackSpace' 'usleep 100' 'keyup Control_L'
 sleep .1
+# Enter do command
 setxkbmap de
 string="do $1/sublime2stata.do"
 xdotool type --delay 1 "${string}"
 sleep .1
+# Execute
 xte 'key Return'
 sleep .3
 
